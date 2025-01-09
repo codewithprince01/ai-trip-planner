bafda40690cf8bd56ddb19f33b46e863457e2458
codewithprince0 <codewithprince0@gmail.com>
2025-01-09 23:08:44 +0530

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Ensure this is set to your Vite build folder.
+    outDir: 'dist',
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    port: 3000,
   },
 });
