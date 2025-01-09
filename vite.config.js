0e2bb0a89295da22eca31f510e270e37f6f1dc93
codewithprince0 <codewithprince0@gmail.com>
2025-01-09 23:06:19 +0530

    hey

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  base: './', // Ensures the app works well with paths in subdirectories or deployment platforms like Vercel.
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this is set to your Vite build folder.
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // Alias path configuration.
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
